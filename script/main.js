cca621b88428b94f1022e274703e09cf2dec932b
Atul Anand <atulcleaver@gmail.com>
2021-04-23 21:31:16 +0530

    added a complete new page music I love to talk about my music  favorites

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-var colors = ['#025fff','#22d278','#f09835','#e9373f','#9650a9'];
+var colors = ['#025fff','#8ac926','#f09835','#e9373f','#9650a9'];
 
 var randomColors = colors[Math.floor(Math.random() * colors.length)];
 
@@ -7,3 +7,14 @@ function setColor(){
 }
 
 setColor();
+
+// This will just a less opacity
+var colors = ['#025fff3f','#8ac9263f','#f098353f','#e9373f3f','#9650a93f'];
+
+var randomColors = colors[Math.floor(Math.random() * colors.length)];
+
+function lightColor(){
+    $(":root").css("--light", randomColors);
+}
+
+lightColor();
