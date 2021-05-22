0bc384ee6ef15e8cd45890f82da286f42bd1b594
Atul Anand <atulcleaver@gmail.com>
2021-05-22 17:57:13 +0530

    changed the logo and font of the page

--- script/main.js
+++ script/main.js
@@ -1,9 +1,23 @@
-const colors = ['#025fff', '#f58634', '#81b214', '#fb3640', '#aa2ee6'];
+const colors = ['#90e0ef', '#ffafcc', '#ffe18a','#76c893','#72efdd','#34a0a4','#deaaff','#52b788'];
 
-const randomColors = colors[Math.floor(Math.random() * colors.length)];
+
+// It gives random number
+const randomNumber = Math.floor(Math.random() * colors.length);
+// It gives random color
+const randomColors = colors[randomNumber];
+
+//Properties
+const opacity = '1.8f';
+
+//Scheme Color
+const schemeColors = randomColors;
+
+
+const backgroundColors = randomColors.concat(opacity);
 
 function setColor(){
-    $(":root").css("--scheme", randomColors);
+    $(":root").css("--scheme", schemeColors);
+    $(":root").css("--background-color", backgroundColors);
 }
 
 setColor();
